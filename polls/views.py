c5432da870284008b2168ad5339fe61de2c682f4
dw2664 <90633701+dw2664@users.noreply.github.com>
2021-10-02 18:48:36 -0400

    fix vote

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
