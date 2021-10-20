e5aad20b5c1bb30d518cb0a0fb8e73f7ac098812
dw2664 <helicopta@users.noreply.github.com>
2021-10-20 12:14:41 -0400

    fix flake8

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 import datetime
 
 # Create your models here.
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
